a881a418f3e5e2898196909ba449ec31793a8cf2
Gilang737 <gs7832583@gmail.com>
2026-02-26 11:42:27 +0700

    mengubah ukuran gambar dengan sekali saja di div

--- app/page.js
+++ app/page.js
@@ -4,72 +4,9 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full flex-col py-12 px-16 items-start">
-        <h1 className="text-6xl font-bold text-center">
+        <h1 className="text-sm font-bold text-center lg:text-5xl">
           Welcome to My Film Database</h1>
-        <div className="mt-6 flex gap-5 flex-wrap items-start justify-center">
-          <Image
-            src="/db.png"
-            alt="Vercel Logo"
-            className="mb-16 w-48 rounded-2xl hover:opacity-75 transition-opacity"
-            width={192}
-            height={48}
-          />
-          <Image
-            src="/db.png"
-            alt="Vercel Logo"
-            className="mb-16 w-48 rounded-2xl hover:opacity-75 transition-opacity"
-            width={192}
-            height={48}
-          />
-          <Image
-            src="/db.png"
-            alt="Vercel Logo"
-            className="mb-16 w-48 rounded-2xl hover:opacity-75 transition-opacity"
-            width={192}
-            height={48}
-          />
-          <Image
-            src="/db.png"
-            alt="Vercel Logo"
-            className="mb-16 w-48 rounded-2xl hover:opacity-75 transition-opacity"
-            width={192}
-            height={48}
-          />
-          <Image
-            src="/db.png"
-            alt="Vercel Logo"
-            className="mb-16 w-48 rounded-2xl hover:opacity-75 transition-opacity"
-            width={192}
-            height={48}
-          />
-          <Image
-            src="/db.png"
-            alt="Vercel Logo"
-            className="mb-16 w-48 rounded-2xl hover:opacity-75 transition-opacity"
-            width={192}
-            height={48}
-          />
-          <Image
-            src="/db.png"
-            alt="Vercel Logo"
-            className="mb-16 w-48 rounded-2xl hover:opacity-75 transition-opacity"
-            width={192}
-            height={48}
-          />
-          <Image
-            src="/db.png"
-            alt="Vercel Logo"
-            className="mb-16 w-48 rounded-2xl hover:opacity-75 transition-opacity"
-            width={192}
-            height={48}
-          />
-          <Image
-            src="/db.png"
-            alt="Vercel Logo"
-            className="mb-16 w-48 rounded-2xl hover:opacity-75 transition-opacity"
-            width={192}
-            height={48}
-          />
+        <div className="mt-8 flex gap-5 flex-wrap items-start justify-center [&_img]:w-24 lg:[&_img]:w-48">
           <Image
             src="/db.png"
             alt="Vercel Logo"
